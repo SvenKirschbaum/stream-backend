09d30e180356e483b3fc8325b9405562fb2944f9
Sven Kirschbaum <sven@kirschbaum.me>
2021-01-11 01:20:28 +0100

    Initialize project using Spring initializr

---
+++ src/main/java/de/elite12/stream/StreamApplication.java
@@ -0,0 +1,13 @@
+package de.elite12.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/elite12/stream/StreamApplicationTests.java
@@ -0,0 +1,13 @@
+package de.elite12.stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
